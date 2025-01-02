4ff21a6bce61d3aa2511a4108778e4919ffe4f90
𝘼𝙧𝙧𝙖𝙬𝙞𝙣 𝙅 <140261508+arrawin@users.noreply.github.com>
2025-01-02 22:34:19 +0530

    Update 0915-57-2559-count-vowel-strings-in-ranges.java

--- 0915-57-2559-count-vowel-strings-in-ranges/0915-57-2559-count-vowel-strings-in-ranges.java
+++ 0915-57-2559-count-vowel-strings-in-ranges/0915-57-2559-count-vowel-strings-in-ranges.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[] vowelStrings(String[] words, int[][] queries) {
-        int[] prefixSum = new int[words.length+1];  
+        int[] prefixSum = new int[words.length+1];  //prefixsum means having a cumulative count , hence size is n+1
         int[] result = new int[queries.length];   
         for(int i = 0 ; i<words.length ; i++){
             String str = words[i];
             int len = str.length();
             if("aeiouAEIOU".indexOf(str.charAt(0)) != -1 && "aeiouAEIOU".indexOf(str.charAt(len - 1)) != -1){
-                 prefixSum[i+1]=prefixSum[i]+1;
+                 prefixSum[i+1]=prefixSum[i]+1; // if we have a vowel cumulative count is increased
             }
             else{
                  prefixSum[i+1]=prefixSum[i];
@@ -16,7 +16,8 @@ class Solution {
         for(int i=0;i<queries.length;i++){
             int start = queries[i][0];
             int end = queries[i][1];
-            result[i]=prefixSum[end+1]-prefixSum[start];
+            result[i]=prefixSum[end+1]-prefixSum[start]; //general formula for prefixsum
+            
     }
 return result;
           
