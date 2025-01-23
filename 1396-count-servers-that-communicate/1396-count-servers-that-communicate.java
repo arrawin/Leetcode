dc186933a45a5834d54cf4a2bcc937ab4ce8f954
𝘼𝙧𝙧𝙖𝙬𝙞𝙣 𝙅 <140261508+arrawin@users.noreply.github.com>
2025-01-23 20:29:46 +0530

    Time: 2 ms (100%), Space: 52.3 MB (69.49%) - LeetHub

--- 1396-count-servers-that-communicate/1396-count-servers-that-communicate.java
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.java
@@ -13,13 +13,12 @@ class Solution {
        }
         for(int i = 0 ; i < grid.length ; i++){
         for(int j = 0 ; j < grid[0].length ; j++){
-            if(grid[i][j]==1){
-                if(rowServer[i]>1 || colServer[j]>1){
+            if(grid[i][j]==1 && (rowServer[i]>1 || colServer[j]>1)){
                     count++;
                 }
             }
         }
-        }
+        
         return count;
         }
 }
