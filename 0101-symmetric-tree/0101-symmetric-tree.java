232ce7ce721ebc074e4ec8b339fc86e13b189bec
𝘼𝙧𝙧𝙖𝙬𝙞𝙣 𝙅 <140261508+arrawin@users.noreply.github.com>
2025-01-27 21:18:04 +0530

    Time: 0 ms (100%), Space: 42.2 MB (20.33%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return solve(root.left, root.right);
+    }
+    boolean solve(TreeNode n1, TreeNode n2){
+        if(n1 == null && n2 == null){
+            return true;
+        }
+        if(n1 == null || n2 == null){
+            return false;
+        }
+        return n1.val == n2.val && solve(n1.left,n2.right) && solve(n1.right,n2.left);
+    }
+}
