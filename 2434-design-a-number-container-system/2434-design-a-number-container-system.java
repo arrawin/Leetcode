e82ce16255c650492cc8cca7576805ec5b2cdd8b
𝘼𝙧𝙧𝙖𝙬𝙞𝙣 𝙅 <140261508+arrawin@users.noreply.github.com>
2025-02-08 21:02:21 +0530

    Time: 85 ms (44.52%), Space: 99.6 MB (43.15%) - LeetHub

---
+++ 2434-design-a-number-container-system/2434-design-a-number-container-system.java
@@ -0,0 +1,45 @@
+class NumberContainers {
+    Map < Integer , Integer > index_val ; 
+    Map < Integer , TreeSet<Integer> > val_index;
+
+    public NumberContainers() {
+        index_val = new HashMap<>();
+        val_index = new HashMap<>();
+        
+    }
+    
+    public void change(int index, int number) {
+        if(index_val.containsKey(index))
+        {
+            int new_val = index_val.get(index);
+            val_index.get(new_val).remove(index);
+            if(val_index.get(new_val).isEmpty())
+            {
+                val_index.remove(new_val);
+            }
+
+        }
+        index_val.put(index,number);
+        TreeSet < Integer > set = val_index.getOrDefault(number , new TreeSet<>());
+        set.add(index);
+        val_index.put (number , set );
+        
+    }
+    
+    public int find(int number) {
+        if(val_index.containsKey(number))
+        {
+            TreeSet < Integer > set = val_index.get(number);
+            return set.first();
+
+        }
+        return -1;
+    }
+}
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers obj = new NumberContainers();
+ * obj.change(index,number);
+ * int param_2 = obj.find(number);
+ */
