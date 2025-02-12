c4e12d317ab9679306e8067b2a81d05a0cfd5153
𝘼𝙧𝙧𝙖𝙬𝙞𝙣 𝙅 <140261508+arrawin@users.noreply.github.com>
2025-02-12 21:46:52 +0530

    Time: 38 ms (80.14%), Space: 63.2 MB (22.32%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.java
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -1,18 +1,13 @@
 class Solution {
-     public int sumOfDigits(int num) {
-         int sum = 0;
-        while (num > 0) {
-            sum += num % 10; 
-            num /= 10;      
-        }
-        return sum;
-    }
-
     public int maximumSum(int[] nums) {
         int max_value = Integer.MIN_VALUE;
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int num : nums) {
-            int digitSum = sumOfDigits(num);
+            int digitSum = 0 , temp = num;
+            while (temp > 0) {
+            digitSum += temp % 10; 
+            temp /= 10;      
+             }
             if (map.containsKey(digitSum)) {
                 max_value = Math.max(max_value, map.get(digitSum) + num);
             }
