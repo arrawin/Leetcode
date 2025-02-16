13a5463e3976609ac0fa53c95c5ff7f1b64fb6e8
𝘼𝙧𝙧𝙖𝙬𝙞𝙣 𝙅 <140261508+arrawin@users.noreply.github.com>
2025-02-16 22:13:17 +0530

    Time: 0 ms (100%), Space: 41.8 MB (30.02%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
@@ -3,18 +3,13 @@ class Solution {
         if(sentence.length()<26){
             return false;
         }
-        int[] freq_count = new int[26];
-        for(int i = 0 ; i < sentence.length() ; i++){
-            int ch = (sentence.charAt(i)-'a');
-            freq_count[ch]++;
-
-        }
-         for(int i = 0 ; i < 26 ; i++){
-                if(freq_count[i]<1){
+         String alphabets = "abcdefghijklmnopqrstuvwxyz";
+        for(int i=0; i<26; i++) {
+            if(sentence.indexOf(alphabets.charAt(i)) == -1) {
                 return false;
             }
-         }
-         return true;
-        
+        }
+        return true;
+    
     }
 }
