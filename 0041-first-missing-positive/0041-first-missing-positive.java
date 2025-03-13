d4f006c5b7faa93b239d70169cfa5357bc761659
𝘼𝙧𝙧𝙖𝙬𝙞𝙣 𝙅 <140261508+arrawin@users.noreply.github.com>
2025-03-13 19:52:39 +0530

    Time: 1 ms (100%), Space: 57.2 MB (64.56%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+
+        int n = nums.length;
+        int curr;
+        boolean arr[] = new boolean[n+1];
+
+        for(int i = 0; i < n; i++) {
+            curr = nums[i];
+            if (nums[i] > 0 && nums[i] <= n) {
+                arr[curr] = true;
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (!arr[i]) {
+                return i;
+            }
+        }
+
+        return n + 1;
+    }
+}
