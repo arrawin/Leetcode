0b257778873175ba1003ee43243aa0a346e8f1ad
𝘼𝙧𝙧𝙖𝙬𝙞𝙣 𝙅 <140261508+arrawin@users.noreply.github.com>
2025-03-15 22:16:13 +0530

    Time: 1 ms (99.98%), Space: 44.6 MB (13.41%) - LeetHub

---
+++ 3723-sum-of-good-numbers/3723-sum-of-good-numbers.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int sumOfGoodNumbers(int[] nums, int k) {
+        int sum = 0,n = nums.length;
+        for(int i=0;i<nums.length;i++){
+            if (i - k >= 0 && nums[i] <= nums[i - k])
+                continue;
+            if (i + k < n && nums[i] <= nums[i + k])
+                continue;
+            sum += nums[i];
+        }
+        return sum;
+    }
+}
