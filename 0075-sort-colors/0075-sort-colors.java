cc74eab796945b7c25e5a32291405e3c61ace0c0
𝘼𝙧𝙧𝙖𝙬𝙞𝙣 𝙅 <140261508+arrawin@users.noreply.github.com>
2025-05-17 19:21:39 +0530

    Time: 0 ms (100%), Space: 42.1 MB (30.03%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -9,15 +9,15 @@ class Solution {
     public void sortColors(int[] nums) {
         int low=0,mid=0,high=nums.length-1;
         while(mid<=high){
-            if (nums[mid]==0){
-            swap(nums,mid,low);
-            mid+=1;
-            low+=1; 
-            } 
-            else if (nums[mid]==1){
-             mid+=1;
+            if(nums[mid]==0){
+                swap(nums,mid,low);
+                mid+=1;
+                low+=1;
             }
-            else {
+            else if(nums[mid]==1){
+                mid+=1;
+            }
+            else{
                 swap(nums,mid,high);
                 high-=1;
             }
