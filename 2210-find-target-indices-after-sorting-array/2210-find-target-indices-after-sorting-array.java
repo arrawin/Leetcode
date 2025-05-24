e5222b3ed896d94bdc8d12d5f63e34bfde6622cf
𝘼𝙧𝙧𝙖𝙬𝙞𝙣 𝙅 <140261508+arrawin@users.noreply.github.com>
2025-05-24 10:55:43 +0530

    Time: 0 ms (100%), Space: 43.4 MB (44.23%) - LeetHub

--- 2210-find-target-indices-after-sorting-array/2210-find-target-indices-after-sorting-array.java
+++ 2210-find-target-indices-after-sorting-array/2210-find-target-indices-after-sorting-array.java
@@ -1,46 +1,21 @@
 class Solution {
-
-    // Function to perform quicksort
-    public static void quickSort(int[] arr, int low, int high) {
-        if (low < high) {
-            int pivotIndex = partition(arr, low, high);  
-            quickSort(arr, low, pivotIndex - 1);         
-            quickSort(arr, pivotIndex + 1, high);        
-        }
-    }
-
-    // Partition function
-    private static int partition(int[] arr, int low, int high) {
-        int pivot = arr[high];  // Choose last element as pivot
-        int i = low - 1;
-
-        for (int j = low; j < high; j++) {
-            if (arr[j] < pivot) {
-                i++;
-                swap(arr, i, j);
-            }
-        }
-
-        swap(arr, i + 1, high);
-        return i + 1;
-    }
-
-    // Swap helper
-    private static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-
     public List<Integer> targetIndices(int[] nums, int target) {
-        quickSort(nums , 0 , nums.length-1);
         List<Integer> list = new ArrayList<>();
-        for(int i = 0 ; i < nums.length ; i++){
-            if(nums[i]==target){
-                list.add(i);
+        int pos = 0;
+        int numThere = 0;
+        for(int num : nums){
+            if(num < target){
+                pos++;     // since we need to find the position of the element we are finding the values lesser than that so we can know which is the position of the target
             }
+            else if(num == target){
+                numThere++;
+            }
+        }
+        while(numThere!=0){
+            list.add(pos);
+            pos++;
+            numThere--;
         }
         return list;
-        
     }
 }
