222a32520d7217733798a259fdd3049413ff0457
𝘼𝙧𝙧𝙖𝙬𝙞𝙣 𝙅 <140261508+arrawin@users.noreply.github.com>
2025-05-25 22:22:35 +0530

    Time: 13 ms (97.36%), Space: 60.9 MB (8.74%) - LeetHub

---
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int longestPalindrome(String[] words) {
+        int ans = 0;
+        int[][] count = new int[26][26];
+        
+        for (String word : words) {
+            int i = word.charAt(0) - 'a';
+            int j = word.charAt(1) - 'a';
+            
+            if (count[j][i] > 0) { // check reverse
+                ans += 4;
+                count[j][i]--;
+            } else {
+                count[i][j]++;
+            }
+        }
+
+        // Check for palindromic center
+        for (int i = 0; i < 26; i++) {
+            if (count[i][i] > 0) {
+                ans += 2;
+                break; // only one palindromic word can be used in the center
+            }
+        }
+
+        return ans;
+    }
+}
