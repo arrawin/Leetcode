5c14f98bef3e1e1cb1a6dbee427af6edc748886b
𝘼𝙧𝙧𝙖𝙬𝙞𝙣 𝙅 <140261508+arrawin@users.noreply.github.com>
2025-06-13 23:40:13 +0530

    Time: 1 ms (99.74%), Space: 46.3 MB (11.08%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,14 +1,20 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int index = 0; 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                int temp = nums[i];
-                nums[i] = nums[index];
-                nums[index] = temp;
-                
-                index++;
+        int n = nums.length;
+        int pos = 0;
+
+        //Traverse through the array . all the non zero elements to its relative position while maintaining order
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i]!=0){
+                nums[pos] = nums[i];
+                pos++;
             }
         }
+        while(pos < nums.length){
+            nums[pos] = 0;
+            pos++;
+        }
+       
     }
 }
+       
